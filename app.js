6e0c4541b6a79d97593ae313d9dc7cb2522ea0cb
brochezapbrasil <brochezap@gmail.com>
2026-07-25 18:19:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       new QRCode(qrDiv, {
         text: link,
         width: 150,
-        height: 150,
+        height: 100,
         correctLevel: QRCode.CorrectLevel.H
       });
     }
