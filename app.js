d711ed50ee75973cec135f525bad4afb6abc1415
brochezapbrasil <brochezap@gmail.com>
2026-07-29 17:39:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -161,9 +161,9 @@ function gerarCertificadoOficial(empresa) {
       const qrData = getQrDataUrl(); if (!qrData) { section.style.display="block"; return; }
       const qr = new Image();
       qr.onload = () => {
-const tamanho = canvas.width * 0.245;
-const x = canvas.width * 0.708;
-const y = canvas.height * 0.138;
+const tamanho = canvas.width * 0.248;
+const x = canvas.width * 0.690;
+const y = canvas.height * 0.133;
         ctx.fillStyle = "#fff"; ctx.fillRect(x-2, y-2, tamanho+4, tamanho+4);
         ctx.drawImage(qr, x, y, tamanho, tamanho);
         if (section) section.style.display = "block";
