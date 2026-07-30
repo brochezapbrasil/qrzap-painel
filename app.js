66db855ffb2562bf0c96e9fa835199044d72c8b0
brochezapbrasil <brochezap@gmail.com>
2026-07-30 18:09:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ const qr = new Image();
     qr.onload = () => {
         const tamanho = canvas.width * 0.42;
         const x = (canvas.width - tamanho) / 2;
-        const y = canvas.height-tamanho)/2;
+        const y = canvas.height*2;
 
         ctx.fillStyle = "#fff";
         ctx.fillRect(x - 2, y - 2, tamanho + 4, tamanho + 4);
