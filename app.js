5b1603842133d9254104114f43ace6b1c4bf12a4
brochezapbrasil <brochezap@gmail.com>
2026-08-06 21:38:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ctx.fillStyle = "#1A2340";
       ctx.textAlign = "left";
       ctx.font = "bold 16px Arial";
-      ctx.fillText(serial, 878, 820);
+      ctx.fillText(serial, 960, 838);
       // ──────────────────────────────────────────────────────────────────
     };
     img.src = "certificado-oficial.png?v=" + Date.now();
